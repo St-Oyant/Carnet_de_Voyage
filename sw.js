f1340e8fc50d5fbc15fa87afe6f0828052f6ba03
St-Oyant <antoine.saintoyant@gmail.com>
2026-07-28 07:02:15 +0200

    Claude V1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "carnet-irlande-v1";
+const CACHE_NAME = "carnet-irlande-v2";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable.png"];
 
 self.addEventListener("install", (e) => {
